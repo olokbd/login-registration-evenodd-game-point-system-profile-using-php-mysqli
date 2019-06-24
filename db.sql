63095f4e6e1b0675b6c842ab8822f94f485150ee
Munawer Mahtab Munna <munnakazi33@gmail.com>
2019-06-25 00:23:06 +0600

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `gender` varchar(5) NOT NULL,
+  `gender` varchar(10) NOT NULL,
   `birth_day` int(4) NOT NULL,
   `birth_month` varchar(4) NOT NULL,
   `birth_year` int(4) NOT NULL,
